9189ee618aa63e9832a5ad2653b0f12c48312d67
MEGA <MEGA@gmail.com>
2025-04-07 13:30:13 +0300

    Add project files.

---
+++ 1zaddd/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1zaddd
+{ }
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Въведете списък от числа, разделени с интервал:");
+        string input = Console.ReadLine();
+
+
+        string[] inputArray = input.Split(' ');
+        List<int> numbers = new List<int>();
+
+        foreach (string str in inputArray)
+        {
+            if (int.TryParse(str, out int num))
+            {
+                numbers.Add(num);
+            }
+        }
+
+
+        HashSet<int> uniqueNumbers = new HashSet<int>(numbers);
+
+
+        Console.WriteLine("Уникални числа:");
+        foreach (var num in uniqueNumbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
+
+
